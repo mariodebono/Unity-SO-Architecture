0f60b532cba1b2ee6d2220ca400e5d63af21f73a
Mario Debono <mario.debono@gmail.com>
2023-05-25 23:49:18 +0200

    fix: incorrect vector3 var type

--- Runtime/Variables/Vector3Variable.cs
+++ Runtime/Variables/Vector3Variable.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 namespace MarioDebono.SOArchitecture.Variables
 {
     [CreateAssetMenu(menuName = "SO Architecture/Variables/Vector3 Variable", fileName = "Vector3 Variable")]
-    public class Vector3Variable : BaseVariable<float>
+    public class Vector3Variable : BaseVariable<Vector3>
     {
     }
 
     [Serializable]
-    public class Vector3Reference : BaseVariableReference<Vector3Variable, float> { }
+    public class Vector3Reference : BaseVariableReference<Vector3Variable, Vector3> { }
 }
