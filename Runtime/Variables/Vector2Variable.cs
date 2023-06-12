e20673414c8f26a7fc534d73ed4c20e532a353cf
Mario Debono <mario.debono@gmail.com>
2023-06-12 23:01:23 +0200

    feat: Add Vector2 variable

---
+++ Runtime/Variables/Vector2Variable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace MarioDebono.SOArchitecture.Variables
+{
+    [CreateAssetMenu(menuName = "SO Architecture/Variables/Vector2 Variable", fileName = "Vector2 Variable")]
+    public class Vector2Variable : BaseVariable<Vector2>
+    {
+    }
+
+    [Serializable]
+    public class Vector2Reference : BaseVariableReference<Vector2Variable, Vector2> { }
+}
